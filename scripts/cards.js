cd12b8e94514878b95969418226931e3d6eed8d7
Ilya Aleksentsev <freedigestone@yandex.ru>
2025-02-27 13:00:58 +0900

    6 экспедиций Кусто

--- scripts/cards.js
+++ scripts/cards.js
@@ -1,26 +1,32 @@
 const initialCards = [
-    {
-      name: "Архыз",
-      link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg",
-    },
-    {
-      name: "Челябинская область",
-      link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg",
-    },
-    {
-      name: "Иваново",
-      link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg",
-    },
-    {
-      name: "Камчатка",
-      link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg",
-    },
-    {
-      name: "Холмогорский район",
-      link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg",
-    },
-    {
-      name: "Байкал",
-      link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg",
-    }
+  {
+    name: "Большая Голубая Дыра Белиза",
+    link: "https://lh3.googleusercontent.com/p/AF1QipNuzyi09dnodFNeJB3I5WfF_dmaWSGfPM_zbow3=s1360-w1360-h1020",
+    alt: "Вид сверху на Большую Голубую Дыру в Белизе — знаменитую подводную воронку с насыщенно-синим цветом",
+  },
+  {
+    name: "Амазонка",
+    link: "https://tengrinews.kz/userdata/images/u40/resized/c83e338fdc5ae400cadfe0d98acc7dfb.jpg",
+    alt: "Река Амазонка и ее леса, вид с высоты птичего полета",
+  },
+  {
+    name: "Большой Барьерный Риф",
+    link: "https://tengrinews.kz/userdata/images/u40/resized/1440ff6f3880e3f235756b6f4a0cc371.JPG",
+    alt: "Большой барьерный риф, вид с высоты птичего полета",
+  },
+  {
+    name: "Антарктида",
+    link: "https://tengrinews.kz/userdata/images/u40/811fcd16cd3b3d987f0ce8648bd7ba65.jpg",
+    alt: "Отколовшийся ледник лазурного цветаб в воде, не далеко от скалистого берега",
+  },
+  {
+    name: "Мадагаскар",
+    link: "https://tengrinews.kz/userdata/images/u40/resized/24c2552cb38e9c194dfc62e55e08d67e.jpg",
+    alt: "Животные пасущиеся на лугу, на  побережье с лазурной водой",
+  },
+  {
+    name: "Байкал",
+    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg",
+    alt: "Скалистый берег озера Байкал",
+  }
 ];
--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,27 @@
-// @todo: Темплейт карточки
+const cardsContainer = document.querySelector('.places__list');
+const cardTemplate = document.querySelector('#card-template').content;
 
-// @todo: DOM узлы
+function createCard(cardData){
+  const cardElement = cardTemplate.cloneNode(true);
+    const card = cardElement.querySelector('.card');
+    const cardImage = cardElement.querySelector('.card__image');
+    const cardTitle = cardElement.querySelector('.card__title');
+    const deleteButton = cardElement.querySelector('.card__delete-button');
+      
+    cardImage.src = cardData.link;
+    cardImage.alt = cardData.alt;
+    cardTitle.textContent = cardData.name; 
+    deleteButton.addEventListener ('click', function(){
+      card.remove();
+    });
+    return card;  
+};
 
-// @todo: Функция создания карточки
+function renderCards(){
+  initialCards.forEach(function(cardData){
+    const cardElement = createCard(cardData);
+    cardsContainer.appendChild(cardElement);
+  });
+};
 
-// @todo: Функция удаления карточки
-
-// @todo: Вывести карточки на страницу
+renderCards();
