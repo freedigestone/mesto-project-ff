0c3305194511946f00561011586a525a5d6e1f6c
Ilya Aleksentsev <freedigestone@yandex.ru>
2025-02-27 21:47:52 +0900

    6 экспедиций Жака Кусто правки

--- scripts/index.js
+++ scripts/index.js
@@ -1,27 +1,31 @@
 const cardsContainer = document.querySelector('.places__list');
 const cardTemplate = document.querySelector('#card-template').content;
 
-function createCard(cardData){
+function createCard(cardData, onDelete) { 
   const cardElement = cardTemplate.cloneNode(true);
-    const card = cardElement.querySelector('.card');
-    const cardImage = cardElement.querySelector('.card__image');
-    const cardTitle = cardElement.querySelector('.card__title');
-    const deleteButton = cardElement.querySelector('.card__delete-button');
-      
-    cardImage.src = cardData.link;
-    cardImage.alt = cardData.alt;
-    cardTitle.textContent = cardData.name; 
-    deleteButton.addEventListener ('click', function(){
+  const card = cardElement.querySelector('.card');
+  const cardImage = cardElement.querySelector('.card__image');
+  const cardTitle = cardElement.querySelector('.card__title');
+  const deleteButton = cardElement.querySelector('.card__delete-button');
+    
+  cardImage.src = cardData.link;
+  cardImage.alt = cardData.name;
+  cardTitle.textContent = cardData.name; 
+
+  deleteButton.addEventListener('click', function() {
+    onDelete(card);
+  });
+
+  return card;  
+}
+
+function renderCards() {
+  initialCards.forEach(function(cardData) {
+    const cardElement = createCard(cardData, function(card) {
       card.remove();
     });
-    return card;  
-};
-
-function renderCards(){
-  initialCards.forEach(function(cardData){
-    const cardElement = createCard(cardData);
-    cardsContainer.appendChild(cardElement);
+    cardsContainer.append(cardElement);
   });
-};
+}
 
 renderCards();
